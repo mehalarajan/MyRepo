3aabfcb6e75998b7dee84f48e06e94773a7405b3
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-03 22:18:11 +0530

    prefix and postfix

---
+++ src/PrifixPostfix.java
@@ -0,0 +1,7 @@
+public class PrifixPostfix {
+    public static void main(String[] args) {
+        int var1 = 7, var2 = 7;
+        System.out.println(var1++);
+        System.out.println(++var2);
+    }
+}
