fd9712fd89f27cf2eb363cb37371d1b11681d687
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-03 23:50:01 +0530

    unary

---
+++ src/unary.java
@@ -0,0 +1,32 @@
+public class unary {
+    public static void main(String[] args)
+    {
+//using unary plus operator
+//represents the positive value of the variable number
+//it is optional
+        int number = +76;
+//prints the positive value of the variable number
+        System.out.println(number);
+
+//using unary minus operator
+//represents the negative value of the variable number
+        number = -number;
+//prints the negative value of the variable number
+        System.out.println(number);
+        int a=20;
+//increments the value of the variable by 1
+        a++;
+//prints the incremented value of the variable
+        System.out.println(a);
+//decrements the value of the variable by 1
+        a--;
+//prints the decremented value of the variable
+        System.out.println(a);
+//declaring a boolean variable
+        boolean flag = false;
+//prints the value of success variable
+        System.out.println(flag);
+//prints the complement of the success variable
+        System.out.println(!flag);
+    }
+}
