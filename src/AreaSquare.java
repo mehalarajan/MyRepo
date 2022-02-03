16f9bdfe919c1ade13349161bc7208d661bdbda2
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-03 22:30:33 +0530

    2.area of square

---
+++ src/AreaSquare.java
@@ -0,0 +1,10 @@
+public class AreaSquare {
+    public static void main (String[] args)
+    {
+        //Value specified in the program itself
+        int side = 4;
+        //Area of Square = side*side
+        int area = side*side;
+        System.out.println("Area of Square is: "+area);
+    }
+}
