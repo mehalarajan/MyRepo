c41fc1f9130e463589cff14b2b18a2493ea1158c
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-03 22:55:30 +0530

    RECTANGLE

---
+++ src/rect.java
@@ -0,0 +1,9 @@
+public class rect {
+    public static void main(String args[])
+    {
+        int width=4;
+        int height=8;
+        int area=width*height;
+        System.out.println("Area of rectangle="+area);
+    }
+}
