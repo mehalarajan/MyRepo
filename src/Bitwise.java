a901e28c13c5fe30a038b551c51ac7fa7ac89fd4
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-04 20:20:21 +0530

    bitwise

---
+++ src/Bitwise.java
@@ -0,0 +1,21 @@
+public class Bitwise {
+    public static void main(String[] args){
+        int x = 9, y = 8;
+        // bitwise and
+        // 1001 & 1000 = 1000 = 8
+        System.out.println("x & y = " + (x & y));
+        // bitwise XOR
+        // 1001 ^ 1000 = 0001 = 1
+        System.out.println("x ^ y = " + (x ^ y));
+        // bitwise inclusive OR
+        // 1001 | 1000 = 1001 = 9
+        System.out.println("x | y = " + (x | y));
+        // bitwise compliment
+        System.out.println("~x = " + (~x));
+        // bitwise rightshift
+        System.out.println("x>>2 = " + (x >>2));
+        //bitwise left shift
+        System.out.println("x<<1 = " + (x << 1));
+    }
+
+}
