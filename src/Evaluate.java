169fb096e6c1d65ac41c341f1e37bd45fe987b2e
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-04 23:15:07 +0530

    Evaluate1

---
+++ src/Evaluate.java
@@ -0,0 +1,8 @@
+public class Evaluate {
+    public static void main(String ...k) {
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true&true);
+        System.out.println(true&false);
+    }
+}
