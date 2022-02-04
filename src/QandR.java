c1d14243a243a33e77f737fbde5974315175d983
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-04 20:48:40 +0530

    QandR

---
+++ src/QandR.java
@@ -0,0 +1,12 @@
+public class QandR {
+    public static void main(String[] args) {
+
+        int dividend = 29, divisor = 4;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
