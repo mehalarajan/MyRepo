d30e8f767817598b4116c743060ce6e8bb7c84e6
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-04 23:20:13 +0530

    Evaluate2

---
+++ src/Evaluate2.java
@@ -0,0 +1,8 @@
+public class Evaluate2 {
+    public static void main(String ...k) {
+        System.out.println(7+7);
+        System.out.println(7+7.0);
+        System.out.println(7+'7');
+    }
+
+}
