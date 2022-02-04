653311f036877b784013873f41d4a800d12dc619
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-04 21:02:53 +0530

    Long

---
+++ src/Long.java
@@ -0,0 +1,9 @@
+public class Long {
+    public static void main(String[] args)
+    {
+        long num1=8975L;
+        long num2=-7896L;
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+    }
+}
