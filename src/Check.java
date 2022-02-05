9b675944b158aca46d1d17c3126e422a22422d7a
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-05 10:19:23 +0530

    long

---
+++ src/Check.java
@@ -0,0 +1,13 @@
+public class Check {
+    public static void main(String[] args) {
+        int a = 6;
+        long b = 6L;
+if(a==b) {
+    System.out.println("true"); //prints true
+}
+else
+{
+    System.out.println(false); //prints false
+}
+    }
+}
