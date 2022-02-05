73535754baae5c0a4ca2227200238ffe40a106f9
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-05 10:22:11 +0530

    double

---
+++ src/Double.java
@@ -0,0 +1,11 @@
+public class Double {
+    public static void main(String[] args) {
+        float a = 6.02f;
+        double b = 6.02d;
+        if (a == b) {
+            System.out.println("true"); //prints true
+        } else {
+            System.out.println(false); //prints false
+        }
+    }
+}
