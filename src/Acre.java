d3b2b9041fb706ed95143448d7632a7630744911
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-07 09:20:10 +0530

    acre

---
+++ src/Acre.java
@@ -0,0 +1,7 @@
+public class Acre {
+    public static void main(String[] args){
+        double squarefeet=250000;
+        double acre = squarefeet/43560;
+        System.out.println(acre);
+    }
+}
--- src/Primesum.java
+++ src/Primesum.java
@@ -3,24 +3,14 @@ public class Primesum {
     public static void main(String[] args) {
 
         int number;
-
-        // create an object of Scanner class
         Scanner sc = new Scanner(System.in);
-
-        // ask users to enter numbers
         System.out.println("Enter a number: ");
         number = sc.nextInt();
 
         boolean flag = false;
         for (int i = 2; i <= number / 2; ++i) {
-
-            // condition for i to be a prime number
             if (checkPrime(i)) {
-
-                // condition for n-i to be a prime number
                 if (checkPrime(number - i)) {
-
-                    // n = primeNumber1 + primeNumber2
                     System.out.printf("%d = %d + %d\n", number, i, number - i);
                     flag = true;
                 }
@@ -36,7 +26,6 @@ public class Primesum {
         sc.close();
     }
 
-    // Function to check prime number
     public static boolean checkPrime(int num) {
         boolean isPrime = true;
 
