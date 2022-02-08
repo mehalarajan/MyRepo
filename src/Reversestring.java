bfcb593b1b50d3537399f08aab0c6a5ebd12cf39
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-08 21:24:20 +0530

    Reversestring

---
+++ src/Reversestring.java
@@ -0,0 +1,14 @@
+public class Reversestring {
+    public static void main(String[] args) {
+        String sentence = "happy";
+        String reversed = reverse(sentence);
+        System.out.println("The reversed sentence is: " + reversed);
+    }
+
+    public static String reverse(String sentence) {
+        if (sentence.isEmpty())
+            return sentence;
+
+        return reverse(sentence.substring(1)) + sentence.charAt(0);
+    }
+}
