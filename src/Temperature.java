312422c1e87da3a7918c0ac48478e7d9242db478
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-08 10:03:40 +0530

    temperature

---
+++ src/Temperature.java
@@ -0,0 +1,7 @@
+public class Temperature {
+    public static void main(String[] args){
+        double celsius = 10.0, fahrenheit = 0.0;
+        fahrenheit = (celsius * 1.8) + 32;
+        System.out.println(" temperature in fahrenheit:"+ fahrenheit);
+    }
+}
