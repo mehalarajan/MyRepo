e19dea081767ad39323ea6a31e004dad8e789ff4
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-10 22:19:02 +0530

    largestnumber

--- src/Largestnum.java
+++ src/Largestnum.java
@@ -1,18 +1,16 @@
 public class Largestnum {
-    public static void main(String[] args)
-    {
-//initializing numbers to compare
-        int a=4, b=89, c=19;
-//comparing numbers, a with b and a with c
-//if both conditions are true, prints a
-        if(a>=b && a>=c)
-            System.out.println(a+" is the largest Number");
-//comparing b with a and b with c
-//if both conditions are true, prints b
-        else if (b>=a && b>=c)
-            System.out.println(b+" is the largest Number");
-        else
-//prints c if the above conditions are false
-            System.out.println(c+" is the largest number");
+    public static void main(String[] args) {
+        int a = 4, b = 89, c = 19;
+        if ( a > b) {
+            if ( a > c)
+                System.out.println(a);
+            else
+                System.out.println(c);
+        } else {
+            if ( b > c)
+                System.out.println(b);
+            else
+                System.out.println(c);
+        }
     }
 }
