e92d360ecdceffe45f3c4ea0a2431ba70b51a996
mehalarajan <98879812+mehalarajan@users.noreply.github.com>
2022-02-11 12:34:53 +0530

    sample8

--- src/Binaryoctal.java
+++ src/Binaryoctal.java
@@ -1,27 +1,12 @@
+import java.util.Scanner;
 public class Binaryoctal {
     public static void main(String[] args) {
-        long binary = 101001;
-        int octal = convertBinarytoOctal(binary);
-        System.out.println(binary + " in binary = " + octal + " in octal");
-    }
-
-    public static int convertBinarytoOctal(long binaryNumber) {
-        int octalNumber = 0, decimalNumber = 0, i = 0;
-
-        while (binaryNumber != 0) {
-            decimalNumber += (binaryNumber % 10) * Math.pow(2, i);
-            ++i;
-            binaryNumber /= 10;
-        }
-
-        i = 1;
-
-        while (decimalNumber != 0) {
-            octalNumber += (decimalNumber % 8) * i;
-            decimalNumber /= 8;
-            i *= 10;
-        }
-
-        return octalNumber;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the binary value");
+       String binary=sc.next();
+       int decimal=Integer.parseInt(binary,2);
+       //System.out.println(decimal);
+       String octal= Integer.toOctalString(decimal);
+       System.out.println(octal);
     }
 }
